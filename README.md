e54fb07dba4bad9beb997f2ae8311512da2d512e
Evandro Delgado <evanbdelgado@gmail.com>
2019-08-14 17:32:26 -0400

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Techdegree_Project_2
+In this project, I use a common web development technique known as "pagination" to enhance the usability of a web page.
+
+Starting with a page that contains a list of 54 student names, photos and email addresses. Them write JavaScript to display 10 names at a time, and add a navigation system -- a series of links at the bottom of the page -- to let users click through the other student entries, 10 at a time. and Them add a search button to search a Student throw the list, if a Student is not listed a messanger will display.
--- js/script.js
+++ js/script.js
@@ -1,50 +1,129 @@
+
+/*jshint esversion: 6 */
+
 /******************************************
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
+
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+// Global variables
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+const page = document.querySelector('.page');
+const fullStudentList = document.querySelectorAll('.student-item');
+const pageHeader = document.querySelector('.page-header');
 
+const searchDiv = document.createElement('div');
+const searchInput = document.createElement('input');
+const pageButtonsDiv = document.createElement('div');
+const pageButtonsUl = document.createElement('ul');
+const noResultsDiv = document.createElement('div');
 
+let searchString = '';
+let filteredStudentList = [];
+const maxStudentsPerPage = 10;
 
+// Append elements for search bar feature
+searchDiv.className = 'student-search';
+searchInput.placeholder = 'Search for students...';
+searchDiv.appendChild(searchInput);
+pageHeader.appendChild(searchDiv);
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
+// Append elements for pagination buttons
+pageButtonsDiv.className = 'pagination';
+pageButtonsDiv.appendChild(pageButtonsUl);
+page.appendChild(pageButtonsDiv);
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+// Append element to display if no students match search
+noResultsDiv.textContent = 'No Match Found';
+noResultsDiv.style.fontSize = '1.5rem';
+noResultsDiv.style.margin = '4rem';
+noResultsDiv.style.textAlign = 'center';
+noResultsDiv.style.display = 'none';
+page.appendChild(noResultsDiv);
 
+// Hide all students in list except for those you want to show
+const showPage = (list, button) => {
+	// Set all students to display none
+	for(let i = 0; i < fullStudentList.length; i++) {
+		fullStudentList[i].style.display = 'none';
+	}
+	// If no results, display message
+	if(list.length === 0) {
+		noResultsDiv.style.display = 'block';
+	}
+	// else display students and the page button selection
+	else {
+		let indexStart = ((button - 1) * maxStudentsPerPage);
+		let indexEnd = (indexStart + maxStudentsPerPage);
+		for(let i = indexStart; i < indexEnd && i < list.length; i++) {
+			list[i].style.display = 'block';
+		}
+		noResultsDiv.style.display = 'none';
+	}
+};
 
+// Generate, append and add functionality to pagination buttons
+const appendPageLinks = (studentList) => {
+	let numStudents = studentList.length;
+	let numPages = Math.ceil(numStudents / maxStudentsPerPage);
+	let selectedButton = 1;
+	pageButtonsUl.innerHTML = '';
 
+	showPage(studentList, selectedButton);
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+	// Create new button for each page
+	for(let i = 0; i < numPages; i++) {
+		let button = document.createElement('li');
+		let anchor = document.createElement('a');
+		anchor.href = '#';
+		// Make first button active
+		if(i === 0) {
+			anchor.className = 'active';
+		}
+		// Buttons text will start at 1 instead of 0
+		anchor.textContent = i + 1;
+		button.appendChild(anchor);
+		pageButtonsUl.appendChild(button);
+	}
 
+	// Update page when new page button clicked
+	pageButtonsUl.addEventListener('click', (e) => {
+		let previousButton = document.querySelector('.active');
+		previousButton.className = '';
+		selectedButton = e.target.textContent;
+		e.target.className = 'active';
+		showPage(studentList, selectedButton);
+	});
+};
 
+// Update filteredStudentList using search and refresh page
+const newQuery = () => {
+	filteredStudentList = [];
+	if(searchString.length === 0) {
+		// Use fullStudentList if searchString is empty
+		appendPageLinks(fullStudentList);
+	} else {
+		// For each item in the full student list
+		for(let i = 0; i < fullStudentList.length; i++) {
+			// Select the student's name as a string
+			let name = fullStudentList[i].querySelector('h3').textContent;
+			// If the student's name contains the search query substring
+			if(name.includes(searchString)) {
+				// Add student item HTML to filteredStudentList
+				filteredStudentList.push(fullStudentList[i]);
+			}
+		}
+		appendPageLinks(filteredStudentList);
+	}
+};
 
+// Call search() function when input value changes
+searchInput.addEventListener('input', () => {
+	searchString = searchInput.value;
+	newQuery();
+});
 
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+// Call when file is initially loaded
+appendPageLinks(fullStudentList);
